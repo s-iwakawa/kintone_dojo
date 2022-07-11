f5e434d5d609de97561da26b99905ac385bb58d8
iwakin53 <107820513+s-iwakawa@users.noreply.github.com>
2022-07-11 15:10:24 +0900

    modified url transition

--- midterm/script.js
+++ midterm/script.js
@@ -12,14 +12,15 @@
     console.log('err', err);
   });
   
+  //取得したレスポンスのdataのみを抽出
   const newsData = getNewsInfo.data;
-  console.log(newsData);
 
-
+  //main > tbl > tblBody の構造を持つように要素を追加
   const main = document.getElementsByTagName('main')[0];
   const tbl = document.createElement("table");
   const tblBody = document.createElement("tbody");
-    
+  
+  //５件取得してきたnewsDataを一件ずつの処理に置き換えて、表を作成する
   const row = Object.keys(newsData).forEach((key) => {
     const tblRow = document.createElement('tr');
     
@@ -28,23 +29,9 @@
     tblDayData.textContent = newsData[key].day.value;
     
     //td要素の作成とカテゴリの書き込み
-    const tblCategoryData = document.createElement('td'); 
+    const tblCategoryData = document.createElement('td');
     tblCategoryData.textContent = newsData[key].category.value;
     
-    //td要素の作成とコンテンツの書き込み(30字以上の内容には…で文字制限する)
-    const tblContentData = document.createElement('td'); 
-    const checkContentLength = () => {
-      const contentLength = newsData[key].content.value.length;
-      const maxLength = 30;
-      if (contentLength > maxLength) {
-        const newContentData = newsData[key].content.value.substr(0, maxLength) + '...'; 
-        tblContentData.textContent = newContentData;
-        return newContentData;
-      }else{
-        tblContentData.textContent = newsData[key].content.value;
-      }
-    };
-
     //categoryによる背景色の条件分岐
     const label = newsData[key].label.value;
     switch (label){
@@ -58,8 +45,33 @@
         tblCategoryData.classList.add('backcolor_ir');
         break;
     };
+
+    //td要素の作成
+    const tblContentData = document.createElement('td');
+
+    //a要素の作成(リンクのため)
+    const hrefInfo = document.createElement('a');
+    hrefInfo.href = newsData[key].url.value; //url.valueに格納されたURLを参照
+    hrefInfo.target = '_blank'; //別ウィンドウで開く
+
+    //30字以上だった場合「…」を追加し、それぞれの条件でa要素にURL情報を付与
+    const checkContentLength = () => {
+      const contentLength = newsData[key].content.value.length;
+      const maxLength = 30;
+      if (contentLength > maxLength) {
+        const newContentData = newsData[key].content.value.substr(0, maxLength) + '...';
+        hrefInfo.textContent = newContentData
+        return newContentData;
+      }else{
+        hrefInfo.textContent = newsData[key].content.value;
+      }
+    };
     
+    //30字チェックの関数の実行
     checkContentLength();
+
+    //要素に各情報を埋め込む
+    tblContentData.appendChild(hrefInfo);
     tblRow.appendChild(tblDayData);
     tblRow.appendChild(tblCategoryData);
     tblRow.appendChild(tblContentData);
