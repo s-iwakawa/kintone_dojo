3c54f7edc4301ea60b51305c4f44bfa0b4384d49
iwakin53 <107820513+s-iwakawa@users.noreply.github.com>
2022-07-15 13:16:00 +0900

    0715

--- midterm/script.js
+++ midterm/script.js
@@ -1,4 +1,4 @@
-(async() => {
+(async () => {
 	'use strict';
 
   const URL = 'https://54o76ppvn8.execute-api.ap-northeast-1.amazonaws.com/prod/bb-dojo';
@@ -11,7 +11,7 @@
   }).catch((err) => {
     console.log('err', err);
   });
-  
+
   //取得したレスポンスのdataのみを抽出
   const newsData = getNewsInfo.data;
 
@@ -19,21 +19,21 @@
   const main = document.getElementsByTagName('main')[0];
   const tbl = document.createElement("table");
   const tblBody = document.createElement("tbody");
-  
+
   //５件取得してきたnewsDataを一件ずつの処理に置き換えて、表を作成する
-  const row = Object.keys(newsData).forEach((key) => {
+  newsData.forEach((key) => {
     const tblRow = document.createElement('tr');
-    
+
     //td要素の作成と日付の書き込み
     const tblDayData = document.createElement('td');
-    tblDayData.textContent = newsData[key].day.value;
-    
+    tblDayData.textContent = key.day.value;
+
     //td要素の作成とカテゴリの書き込み
     const tblCategoryData = document.createElement('td');
-    tblCategoryData.textContent = newsData[key].category.value;
-    
+    tblCategoryData.textContent = key.category.value;
+
     //categoryによる背景色の条件分岐
-    const label = newsData[key].label.value;
+    const label = key.label.value;
     switch (label){
       case 'company':
         tblCategoryData.classList.add('backcolor_company');
@@ -51,22 +51,21 @@
 
     //a要素の作成(リンクのため)
     const hrefInfo = document.createElement('a');
-    hrefInfo.href = newsData[key].url.value; //url.valueに格納されたURLを参照
-    hrefInfo.target = '_blank'; //別ウィンドウで開く
+    hrefInfo.href = key.url.value; //url.valueに格納されたURLを参照
+    hrefInfo.target = key.target.value;
 
     //30字以上だった場合「…」を追加し、それぞれの条件でa要素にURL情報を付与
     const checkContentLength = () => {
-      const contentLength = newsData[key].content.value.length;
       const maxLength = 30;
-      if (contentLength > maxLength) {
-        const newContentData = newsData[key].content.value.substr(0, maxLength) + '...';
+      if (key.content.value.length > maxLength) {
+        const newContentData = key.content.value.substr(0, maxLength) + '...';
         hrefInfo.textContent = newContentData
         return newContentData;
       }else{
-        hrefInfo.textContent = newsData[key].content.value;
+        hrefInfo.textContent = key.content.value;
       }
     };
-    
+
     //30字チェックの関数の実行
     checkContentLength();
 
@@ -79,5 +78,5 @@
     tbl.appendChild(tblBody);
     main.appendChild(tbl);
   });
-})();  
+})();
 
