e1fa3a9f3e29166b1fe283f673b74e92718122c3
iwakin53 <107820513+s-iwakawa@users.noreply.github.com>
2022-07-15 13:41:05 +0900

    0715 1st review

--- Task07/script.js
+++ Task07/script.js
@@ -1,6 +1,6 @@
 (() => {
-'use strict';
-	
+	'use strict';
+
 	//ドロップダウン内の製品と対応する製品コードの定義
 	const dropdownData = {
 		'kintone': 'KN',
@@ -12,7 +12,7 @@
 	const avoidDuplication = (event) => {
 		//日付からハイフンを抜いた文字列を作成
 		const dateData =() => {
-			const dateStr = event.record.日付.value;	
+			const dateStr = event.record.日付.value;
 			return dateStr.replace(/-/g, '');
 		} ;
 
@@ -25,23 +25,22 @@
 		//重複禁止項目に入力する文字列の作成
 		event.record.重複禁止項目_文字列.value = dateData() + '-' + productCode + '-' + manageNumber;
 
-		//重複禁止項目の編集を不可にする
-		event.record.重複禁止項目_文字列.disabled = true;	
-
 		return event;
 	};
 
-	//新規レコード作成時に日付、製品、管理番号が変更されたら関数を実行する
-	kintone.events.on('app.record.create.change.日付', avoidDuplication);
-	kintone.events.on('app.record.create.change.サイボウズ製品', avoidDuplication);
-	kintone.events.on('app.record.create.change.管理番号', avoidDuplication);
-	
-	
-	//レコード編集時に日付、製品、管理番号が変更されたら関数を実行する
-	kintone.events.on('app.record.edit.change.日付', avoidDuplication);
-	kintone.events.on('app.record.edit.change.サイボウズ製品', avoidDuplication);
-	kintone.events.on('app.record.edit.change.管理番号', avoidDuplication);
+	//新規レコード、レコード編集画面になった時に、重複禁止項目の編集を不可にする
+	kintone.events.on(['app.record.create.show', 'app.record.edit.show'], ((event) => {
+		event.record.重複禁止項目_文字列.disabled = true;
+		return event;
+	}));
 
-
-
+	//新規レコード作成時およびレコード編集時に日付、製品、管理番号が変更されたら関数を実行する
+	kintone.events.on([
+		'app.record.create.change.日付',
+		'app.record.create.change.サイボウズ製品',
+		'app.record.create.change.管理番号',
+		'app.record.edit.change.日付',
+		'app.record.edit.change.サイボウズ製品',
+		'app.record.edit.change.管理番号'
+	], avoidDuplication);
 })();
