93a020de94cd9647926a307da93eb643f1e7921c
iwakin53 <107820513+s-iwakawa@users.noreply.github.com>
2022-07-15 09:34:40 +0900

    0715 added query parameter

--- Task09/script.js
+++ Task09/script.js
@@ -3,42 +3,33 @@
 
 	const avoidDuplication = (event) => {
 		const apiURL = kintone.api.url('/k/v1/records.json'); //https://…　ではなく、kintone.api.url() を使うことで短縮できる
+		//新規レコード作成をデフォルトとしてparameterの変数を定義
+		const paramsField = (event.type === 'app.record.create.submit') ? ['重複禁止項目'] : ['重複禁止項目', '$id'];
+		const paramsQuery = (event.type === 'app.record.create.submit') ? `重複禁止項目 = "${event.record.重複禁止項目.value}"` : `重複禁止項目 = "${event.record.重複禁止項目.value}" and $id not in ("${event.record.$id.value}")`;
 		const getParams = {
-			"app": 51,
+			app: kintone.app.getId(),
+			fields: paramsField,
+			paramsQuery,
+			totalCount: true
 		};
 
 		return kintone.api(apiURL, 'GET', getParams).then((response) => {
-			//アプリ内に保存されたレコードの参照
-			const lookupArray = response.records;
-
-			//参照したデータ配列の数、処理を繰り返す
-			for (let count = 0; count <= lookupArray.length; count++) {
-				//比較する重複禁止項目の文字列の定義
-				const currentData = event.record.重複禁止項目.value;
-				const chkStr = lookupArray[count].重複禁止項目.value;
-
-				//文字列の比較での条件分岐
-				if (currentData === chkStr) {
-					//他のレコードに重複があった際に、そのまま保存するか保存をキャンセルするか
-					if (window.confirm('項目が重複しています。このまま保存しますか？')) {
-						window.alert('保存しました');
-						break;
-					}else{
-						 event.error = chkStr + 'は重複しているため、保存キャンセルしました。'
-						break
-					}
-				}else{
-					return ;
-				};
-			};
+			//GETリクエストで値が入っていない場合（他の重複禁止項目と重複していない）そのまま保存イベントを終了
+			if (response.totalCount === '0') return event;
+			//他のレコードに重複があった際に、そのまま保存するか保存をキャンセルするか確認する
+			if (window.confirm('項目が重複しています。このまま保存しますか？')) {
+				window.alert('保存しました');
+			}else{
+				event.error = event.record.重複禁止項目.value + 'は重複しているため、保存キャンセルしました。'; //falseの場合はerrorを書き込む
+			}
 			return event;
 		});
 	};
 
-	//新規レコード作成の保存時に上記関数を実行
-	kintone.events.on('app.record.create.submit', avoidDuplication);
-
-	//レコード編集の保存時に上記関数を実行
-	kintone.events.on('app.record.edit.submit', avoidDuplication);
+	//新規レコード作成の保存時、およびレコード編集の保存時に上記関数を実行
+	kintone.events.on([
+		'app.record.create.submit',
+		'app.record.edit.submit'
+	],avoidDuplication);
 
 })();
